0bfb25c82d1277855d3b52c13f3d32caa2a23838
Srinivasan Rajagopal <60819235+Srinivasan235@users.noreply.github.com>
2021-04-25 19:13:50 +0530

    Add files via upload

---
+++ homePage.js
@@ -0,0 +1,29 @@
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+  $('#recipeCarousel').carousel({
+    interval: 10000
+  })
+  
+  $('.carousel-review .carousel-item').each(function(){
+      var minPerSlide = 3;
+      var next = $(this).next();
+      if (!next.length) {
+      next = $(this).siblings(':first');
+      }
+      next.children(':first-child').clone().appendTo($(this));
+      
+      for (var i=0;i<minPerSlide;i++) {
+          next=next.next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          
+          next.children(':first-child').clone().appendTo($(this));
+        }
+  });
+  
